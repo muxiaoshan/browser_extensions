00fd879e716dd6473920d4eb7fb0df1c61c564ea
linfs <muxiaoshan@126.com>
2017-09-14 18:00:11 +0800

    增加chrome_close插件

---
+++ chrome_close/background.js
@@ -0,0 +1,17 @@
+﻿chrome.runtime.onMessageExternal.addListener(
+    function (request, sender, sendResponse) {
+        
+        console.debug("监听页面发送来的消息");
+        console.debug(request);
+        console.debug(sender);
+        if (request && request.operation == "closeCurrentTab") {
+            var tabId = sender.tab.id;
+            chrome.tabs.remove(tabId);
+        }
+        sendResponse({
+            success: true,
+            closed: true,
+        });
+        //return true;
+    }
+);
